b9fd298efd874cf78b6ec47793e3491aa79bb1ce
MikeHGO <michaelhgo@gmail.com>
2021-10-26 16:32:59 -0300

    pontuacoes em int

--- src/components/SimpleTable.js
+++ src/components/SimpleTable.js
@@ -9,62 +9,68 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
 const StyledTableCell = withStyles((theme) => ({
-	head: {
-		fontSize: 18,
-		border: "none",
-	},
-	body: {
-		fontSize: 18,
-		border: "none",
-	},
+  head: {
+    fontSize: 18,
+    border: "none",
+  },
+  body: {
+    fontSize: 18,
+    border: "none",
+  },
 }))(TableCell);
 
 const StyledTableRow = withStyles((theme) => ({
-	root: {
-		"&:nth-of-type(odd)": {
-			backgroundColor: theme.palette.action.hover,
-		},
-		"&:nth-of-type(-n+8)": {
-			backgroundColor: "lightblue",
-		},
-	},
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    "&:nth-of-type(-n+8)": {
+      backgroundColor: "lightblue",
+    },
+  },
 }))(TableRow);
 
 export default function SimpleTable({ categoria, data }) {
-	return (
-		<TableContainer
-			component={Paper}
-			elevation={0}
-			style={{ marginBottom: "0rem" }}
-		>
-			<Table aria-label="customized table">
-				<TableHead>
-					<TableRow>
-						<StyledTableCell align="center">Posição</StyledTableCell>
-						<StyledTableCell align="center">Jogador</StyledTableCell>
-						{categoria === "PRESENCIAL" ? null : (
-							<StyledTableCell align="center">ID do Tenhou</StyledTableCell>
-						)}
-						<StyledTableCell align="center">Pontos</StyledTableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{data.map((row, index) => (
-						<StyledTableRow key={index}>
-							<StyledTableCell align="center" component="th" scope="row">
-								{index + 1}
-							</StyledTableCell>
-							<StyledTableCell align="center">{row[1]}</StyledTableCell>
-							{categoria === "PRESENCIAL" ? null : (
-								<StyledTableCell align="center">{row[2]}</StyledTableCell>
-							)}
-							<StyledTableCell align="center">
-								{categoria === "PRESENCIAL" ? row[2] : row[3]}
-							</StyledTableCell>
-						</StyledTableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+  return (
+    <TableContainer
+      component={Paper}
+      elevation={0}
+      style={{ marginBottom: "0rem" }}
+    >
+      <Table aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="center">Posição</StyledTableCell>
+            <StyledTableCell align="center">Jogador</StyledTableCell>
+            {categoria === "PRESENCIAL" ? null : (
+              <StyledTableCell align="center">ID do Tenhou</StyledTableCell>
+            )}
+            <StyledTableCell align="center">Pontos</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, index) => (
+            <StyledTableRow key={index}>
+              {/* Coluna Posicao */}
+              <StyledTableCell align="center" component="th" scope="row">
+                {index + 1}
+              </StyledTableCell>
+              {/* Coluna Jogador */}
+              <StyledTableCell align="center">{row[1]}</StyledTableCell>
+              {/* Coluna ID do Tenhou */}
+              {categoria === "PRESENCIAL" ? null : (
+                <StyledTableCell align="center">{row[2]}</StyledTableCell>
+              )}
+              {/* Coluna Pontos */}
+              <StyledTableCell align="center">
+                {categoria === "PRESENCIAL"
+                  ? Math.floor(row[2])
+                  : Math.floor(row[3])}
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
